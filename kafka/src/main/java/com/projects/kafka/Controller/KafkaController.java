c4834f4549c031443c163d23a459e3ca8aa5a3b8
ysbc1247 <ystc1247@khu.ac.kr>
2023-11-27 04:44:11 +0900

    add api for simple kafka message test

---
+++ kafka/src/main/java/com/projects/kafka/Controller/KafkaController.java
@@ -0,0 +1,21 @@
+package com.projects.kafka.Controller;
+
+import com.projects.kafka.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaController {
+
+    @Autowired
+    private KafkaProducerService producerService;
+
+    @PostMapping("/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        producerService.sendMessage("test", message);
+    }
+}
